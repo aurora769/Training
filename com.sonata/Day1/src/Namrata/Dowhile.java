742ac57dd67efcec0533a698633200c56778cd31
Namrata <namrataarora@sonata-software.com>
2021-12-03 04:56:48 +0530

    Modified

---
+++ com.sonata/Day1/src/Namrata/Dowhile.java
@@ -0,0 +1,16 @@
+package Namrata;
+
+public class Dowhile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=1;
+		do {
+			System.out.println("Hi everyone");
+			i++;
+		}
+		while(i<6);
+
+	}
+
+}
