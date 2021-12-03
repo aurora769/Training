2570c0f8d5eb823ceba9c1998c3e8b13cc9f339d
Namrata <namrataarora@sonata-software.com>
2021-12-03 05:58:44 +0530

    Commited

---
+++ Core Java/Day1/src/Namrata/While.java
@@ -0,0 +1,15 @@
+package Namrata;
+
+public class While {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=1;
+		while(i<6) {
+			System.out.println("Hi Namrata");
+			i++;
+		}
+
+	}
+
+}
