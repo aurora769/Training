0fcee32f9a4fd9de786e8746914ac83e024faf1e
Namrata <namrataarora@sonata-software.com>
2021-12-03 05:40:05 +0530

    Added

---
+++ Core Java/Day1/src/Namrata/If.java
@@ -0,0 +1,20 @@
+package Namrata;
+
+public class If {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=10;
+		if(i<20)
+		{
+			System.out.println("10 is less than 20");
+			
+		}
+		else
+		{
+			System.out.println("not less");
+		}
+
+	}
+
+}
---
+++ Core Java/Day1/src/Namrata/Nestedloop.java
@@ -0,0 +1,18 @@
+package Namrata;
+
+public class Nestedloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0;i<=3;i++)
+		{
+			for(int j=0;j<=5;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
