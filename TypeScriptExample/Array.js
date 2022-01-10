cb77e3fa7fc9516c4a5cbf1cd372deca2dc27828
Namrata <53650507+aurora769@users.noreply.github.com>
2022-01-10 19:18:15 +0530

    Update Array.js

--- TypeScriptExample/Array.js
+++ TypeScriptExample/Array.js
@@ -1,7 +1,27 @@
-var fruits = ['Apple', 'Orange', 'Banana'];
-for (var index in fruits) {
-    console.log(fruits[index]); 
-}
-for (var i = 0; i < fruits.length; i++) {
-    console.log(fruits[i]); 
-}
+var Arrayex = /** @class */ (function () {
+    function Arrayex() {
+        this.numarr = [1, 2, 3, 4];
+        this.count = 0;
+    }
+    Arrayex.prototype.ReverseArray = function () {
+        this.numarr.reverse();
+        console.log(this.numarr);
+    };
+    Arrayex.prototype.find = function () {
+        for (this.i = 0; this.i < this.numarr.length; this.i = this.i + 1) {
+            if (5 == this.numarr[this.i]) {
+                this.count = 1;
+            }
+        }
+        if (this.count == 1) {
+            console.log("number found");
+        }
+        else {
+            console.log("number not found");
+        }
+    };
+    return Arrayex;
+}());
+var arr = new Arrayex();
+arr.find();
+arr.ReverseArray();
