e12fd967ecef1a4cabc51acf8b994ba17a7e1bcb
Namrata <53650507+aurora769@users.noreply.github.com>
2022-01-10 19:17:04 +0530

    Update Array.ts

--- TypeScriptExample/Array.ts
+++ TypeScriptExample/Array.ts
@@ -1,11 +1,42 @@
-let fruits: string[] = ['Apple', 'Orange', 'Banana']; 
+class Arrayex{
 
-for(var index in fruits)
-{ 
-    console.log(fruits[index]);  
+    numarr:number[]=[1,2,3,4];
+
+    i:number;
+
+    count:number = 0;
+    ReverseArray(){
+        this.numarr.reverse();
+        console.log(this.numarr);
+    }
+    find(){
+        
+        for(this.i=0;this.i<this.numarr.length;this.i=this.i+1){
+
+            if (5==this.numarr[this.i]){
+
+                this.count =1;
+
+            }
+            
+
+        }
+        if (this.count == 1){
+
+            console.log("number found");
+
+        }
+        else {
+            console.log("number not found");
+        }
+    }
+
+
+
+
 }
 
-for(var i = 0; i < fruits.length; i++)
-{ 
-    console.log(fruits[i]);
-}
+var arr=new Arrayex();
+
+arr.find();
+arr.ReverseArray();
